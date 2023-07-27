84175c567f6cae4ee7c213297faa18d5e9a91136
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-07-28 02:11:24 +0300

    take two: using binomial coefficient method

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,23 +2,30 @@
 """ pascal triangle challenge """
 
 
+"""
 def pascal_triangle(n):
-    """
-    returns a list of lists of integers representing the
-    Pascal’s triangle of n
-    """
     rs = []
     if n <= 0:
         print(rs)
         return rs
     else:
         for i in range(n):
-            """
-            use power method.
-            convert integer to string.
-            split integer to digits.
-            convert back to integer.
-            append to list
-            """
             rs.append([int(j) for j in str(11**i)])
     return rs
+
+
+pascal_triangle(5)
+"""
+
+def pascal_triangle(n):
+    """ Pascal triangle using binomial coefficients """
+    all = []
+    if n <= 0:
+        return []
+    else:
+        for i in range(n):
+            row = [1] #initialize the row with 1
+            for j in range(i):
+                row.append(row[j] * (i - j) // (j + 1))  #binomial coefficient
+            all.append(row) #append the row to the triangle
+        return all #return the triangle
