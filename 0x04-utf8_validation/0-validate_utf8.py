f64b390fa7171789b96d815ac933a592ba9d6b92
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-25 03:21:48 +0300

    add comments

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,13 +3,14 @@
 
 
 def validUTF8(data):
-    if type(data) is not list:
+    """Determines if a given data set represents a valid UTF-8 encoding"""
+    if type(data) is not list: # data is a list of integers
         return False
     for i in data:
-        if type(i) is not int:
+        if type(i) is not int: # each integer represents 1 byte of data
             return False
     try:
-        bytes(i & 0xFF for i in data).decode()
+        bytes(i & 0xFF for i in data).decode() # decode bytes to string
         return True
-    except UnicodeDecodeError:
+    except UnicodeDecodeError: # if data is not valid UTF-8
         return False
